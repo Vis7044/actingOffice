efce7df03b46d1b9e00226473aedbd3e9519786c
Vis7044 <singhvishal7044@gmail.com>
2025-07-18 20:02:14 +0530

    improved backend

--- backend/Controllers/AdminController.cs
+++ backend/Controllers/AdminController.cs
@@ -11,15 +11,36 @@ namespace backend.Controllers
     public class AdminController : ControllerBase
     {
         private readonly IAuthService _authService;
-        public AdminController(IAuthService authService)
+        public readonly IQuoteService _qouteService;
+        public AdminController(IAuthService authService, IQuoteService qouteService)
         {
             _authService = authService;
+            _qouteService = qouteService;
         }
 
         [HttpGet("admin")]
         public async Task<IActionResult> Test()
         {
             return Ok("Access granted to admin endpoint."); 
+        }
+        [HttpGet("users-quote-status")]
+        public async Task<IActionResult> GetQuoteStatus([FromQuery] int page, [FromQuery]int pageSize)
+        {
+            return Ok(await _qouteService.GetUsersQuoteStatus(page,pageSize));
+        }
+
+        [HttpGet("daily-quote-stats")]
+        public async Task<IActionResult> GetQuoteStats([FromQuery] int offset)
+        {
+            var result = await _qouteService.GetDailyQuoteDetails(offset);
+            return Ok(result);
+        }
+
+        [HttpGet("user-quote-amount-stats")]
+        public async Task<IActionResult> GetUserQuoteAmountStats([FromQuery] int offset)
+        {
+            var result = await _qouteService.GetUsersQuoteAmountStatus(offset);
+            return Ok(result);
         }
     }
 }
---
+++ backend/Dtos/QuoteDto/QuoteSummaryDto.cs
@@ -0,0 +1,31 @@
+﻿    namespace backend.Dtos.QuoteDto
+{
+    public class QuoteSummaryDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public int TotalCount { get; set; }
+        public int AcceptedCount { get; set; }
+        public int DraftCount { get; set; }
+        public int RejectedCount { get; set; }
+    }
+
+    public class DailyQuoteSummary
+    {
+        public string _id { get; set; }
+        public int total { get; set; }
+    }
+
+    public class DailyQouteAmountSalary
+    {
+
+        public string DraftCountAmount { get; set; } = string.Empty;
+        public string AcceptedCountAmount { get; set; } = string.Empty;
+        public string TotalCountAmount { get; set; } = string.Empty;
+
+        public string RejectedCountAmount { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
